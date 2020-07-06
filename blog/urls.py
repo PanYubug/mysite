88574e582e3ec118a100a3907ba99c5edc6361a0
panyu <344146473@qq.com>
2020-07-07 00:27:59 +0800

    分页

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,6 @@ import blog.views
 urlpatterns = [
     path('hello', views.hello_world),
     path('content', views.article_content),
-    path('index', blog.views.get_index_page)
+    path('index', blog.views.get_index_page),
+    path('detail/<int:article_id>', blog.views.get_detail_page)
 ]
